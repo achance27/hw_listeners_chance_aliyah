51c6de44b3142bbc8d06679d97342a49af3c3360
aliyahchance <aliyahachance@gmail.com>
2018-03-21 12:16:55 -0400

    Basic event listener applied

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+var pageHeading = document.getElementsByTagName('h1')[0];
+
+pageHeading.addEventListener('click', function () {
+    var myInfo = document.createElement('p');
+    myInfo.innerHTML = '<p>This is click number + x</p>';
+    document.getElementById('container').appendChild(myInfo);
+});
+*/
+
+var pageHeading = document.getElementsByTagName('h1')[0];
+
+pageHeading.addEventListener('click', function () {
+    var myInfo = document.createElement('p');
+    myInfo.innerHTML = '<p>This is click number + x</p>';
+    document.getElementById('container').appendChild(myInfo);
+});

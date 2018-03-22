fc88af8bfcf0bb423f540d6dd8f97bae8c9ae30f
aliyahchance <aliyahachance@gmail.com>
2018-03-22 13:20:21 -0400

    Header informs of number of clicks

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
-/*
 var pageHeading = document.getElementsByTagName('h1')[0];
+var amount = 1;
 
 pageHeading.addEventListener('click', function () {
     var myInfo = document.createElement('p');
-    myInfo.innerHTML = '<p>This is click number + x</p>';
-    document.getElementById('container').appendChild(myInfo);
-});
-*/
-
-var pageHeading = document.getElementsByTagName('h1')[0];
-
-pageHeading.addEventListener('click', function () {
-    var myInfo = document.createElement('p');
-    myInfo.innerHTML = '<p>This is click number + x</p>';
+    myInfo.innerHTML = 'This is click number' + ' ' + amount;
+    amount++;
     document.getElementById('container').appendChild(myInfo);
 });

dd9d1a2c26fc48e87aa11213c5d696744af01efe
aliyahchance <aliyahachance@gmail.com>
2018-03-22 12:35:59 -0400

    Trials of alternate submit button

--- js/main.js
+++ js/main.js
@@ -17,32 +17,8 @@ var submitButton = document.getElementsByTagName('input');
 submitButton.addEventListener('submit', function (eventInfo) {
     eventInfo.preventDefault();
     console.log(eventInfo);
-});
+}); */
 
 document.getElementById('first').addEventListener('submit', function (event) {
     event.preventDefault();
-}); */
-
-/*var AdvWeb2 = {
-    instructor: {
-        fullName: 'Richard Kalehoff'
-    },
-    name: 'Advanced Web 2',
-    numberOfStudents: 16,
-    describeClass: function howdy() {
-        console.log('There are' + AdvWeb2.numberOfStudents + 'students in' + AdvWeb2.name + '.');
-    },
-    students: [
-        {name: 'Sheina M', ufID: 112233, grades: [2, 1, 3]},
-        {name: 'Emily A', ufID: 445566, grades: [5, 4, 6]},
-        {name: 'Kevin A', ufID: 778899, grades: [8, 7, 9]},
-        {name: 'Rebecca H', ufID: 998877, grades: [9, 8, 7]},
-        {name: 'Libby R', ufID: 665544, grades: [6, 5, 4]},
-        {name: 'Tatiana C', ufID: 332211, grades: [3, 2, 1]},
-    ]
-};
-var result = AdvWeb2.students[5].grades */
-
-for (var i = 0; i < 10; i++) {
-    console.log('The value of i is:' + i);
-}
+});

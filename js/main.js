1574d4061c8384413057a3250d0b203ef1940829
aliyahchance <aliyahachance@gmail.com>
2018-03-23 16:02:25 -0400

    Functionable alternate submit button

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
+// Form with alternate submit button
 var userFirst = document.getElementById('first');
 var userLast = document.getElementById('last');
-var contactInfo = document.getElementById('email');
+var email = document.getElementById('email');
 var message = document.getElementById('message');
 
-document.addEventListener('sumbit', function () {
-    console.log('The firstName is:' + '' + userFirst.value);
-    console.log('The lastName is:' + '' + userLast.value);
-    console.log('The email is:' + '' + contactInfo.value);
-    console.log('The message is:' + '' + message.value);
+document.addEventListener('submit', function () {
+    console.log('The firstName is:' + ' ' + userFirst.value);
+    console.log('The lastName is:' + ' ' + userLast.value);
+    console.log('The email is:' + ' ' + email.value);
+    console.log('The message is:' + ' ' + message.value);
     event.preventDefault();
 });

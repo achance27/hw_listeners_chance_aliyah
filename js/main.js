9ddb558b06c2ccb33c97c7f4c070e112d5674cf3
aliyahchance <aliyahachance@gmail.com>
2018-03-23 19:10:39 -0400

    Humor additions to custom

--- js/main.js
+++ js/main.js
@@ -48,15 +48,20 @@ moreInfo.addEventListener('dblclick', function () {
 });
 */
 
-// Where users double click on the "Not really..." button the background changes color revealing a message
+// Where users double click on the "Not really..." button the background changes color revealing a message and a baby meme
 document.getElementById('no').addEventListener('dblclick', function () {
     document.body.style.backgroundColor = 'black';
 });
+document.getElementById('no').addEventListener('dblclick', function () {
+    var ohReally = document.createElement('p');
+    ohReally.innerHTML = '<img src="./img/ohreally.jpg" alt="Oh Really Meme" id="really">';
+    document.getElementsByTagName('p')[1].appendChild(ohReally);
+});
 
-// Users that click on the "Yes, definitely!" button are given a link for more weird laws
+// Users that click on the "Yes, definitely!" button are given a link for more weird laws and a meme
 document.getElementById('yes').addEventListener('click', function () {
-    var learn = document.createElement('p');
-    learn.innerHTML = '<p>click <a href="http://www.dumblaws.com/"> here<a> for more laws!</p>';
+    var learn = document.createElement('article');
+    learn.innerHTML = '<p>click <a href="http://www.dumblaws.com/"> here<a> for more laws!</p><img src="./img/nologic.jpg" alt="no logic meme" id="logic">';
     document.getElementsByTagName('h2')[3].appendChild(learn);
 });
 
@@ -78,5 +83,12 @@ selection.addEventListener('select', function () {
 var learn = document.getElementById('yes');
 button.addEventListener('click', function () {
 
+}
+
+
+window.addEventListener('resize', function () {
+    function image() {
+			var meme =
+		}
 }
 */
